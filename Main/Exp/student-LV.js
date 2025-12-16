535a27e2ec82d950e4a2070504d0ef524bb9a1c8
Jay <83687427+kuyaaj0@users.noreply.github.com>
2025-12-15 21:33:10 -0800

    Update student-LV.js

--- Main/Exp/student-LV.js
+++ Main/Exp/student-LV.js
@@ -1,1 +1,6 @@
-
+export function getStudentLevelData(xp) {
+  if (xp >= 3000) return { level: 15, title: "Learning Legend" };
+  if (xp >= 1500) return { level: 10, title: "Mind Master" };
+  if (xp >= 500) return { level: 5, title: "Knowledge Explorer" };
+  return { level: 1, title: "Beginner" };
+}
